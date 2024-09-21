9af0e5cc8beaa7d055f3b7cfdc04f7d7404d8696
OuyoussMeryem <ouyss.meryem@gmail.com>
2024-09-21 17:06:37 +0100

    first commit

---
+++ src/main/java/com/example/springjenkinsgithub/SpringJenkinsGithubApplication.java
@@ -0,0 +1,25 @@
+package com.example.springjenkinsgithub;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsGithubApplication {
+    public static Logger logger= LoggerFactory.getLogger(SpringJenkinsGithubApplication.class);
+
+    @PostConstruct
+    public void intt(){
+        logger.info("Application  started.....");
+    }
+
+    public static void main(String[] args) {
+
+        logger.info("Application  started.....");
+        SpringApplication.run(SpringJenkinsGithubApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/springjenkinsgithub/SpringJenkinsGithubApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springjenkinsgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsGithubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+      assertEquals(true,true);
+    }
+
+}
